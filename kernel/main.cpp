5616c490fd555da89be3b28c65873d9bcf411ecf
YusukeSK <62171006+YusukeSK@users.noreply.github.com>
2021-08-30 15:36:16 +0900

    day05f printk

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -25,8 +25,28 @@ void operator delete(void* obj) noexcept {
 }
 // #@@range_end(placement_new)
 
+// #@@range_begin(buf)
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
+
+char console_buf[sizeof(Console)];
+Console* console;
+// #@@range_end(buf)
+
+// #@@range_begin(printk)
+int printk(const char* format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
+// #@@range_end(printk)
 
 // #@@range_begin(call_write_pixel)
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
@@ -46,13 +66,15 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       pixel_writer->Write(x, y, {255, 255, 255});
     }
   }
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+  
+  // #@@range_begin(new_console)
+  console = new(console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+  // #@@range_end(new_console)
 
-  char buf[128];
+  // #@@range_begin(use_printk)
   for (int i = 0; i < 27; ++i) {
-    sprintf(buf, "line %d\n", i);
-    console.PutString(buf);
+    printk("printk: %d\n", i);
   }
-
+  // #@@range_end(use_printk)
   while (1) __asm__("hlt");
 }
