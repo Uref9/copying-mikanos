9585a844a77e1ed1f2e944c0459591bcd557cbee
YusukeSK <62171006+YusukeSK@users.noreply.github.com>
2021-08-30 16:36:24 +0900

    day06a draw mouse cursor and desktop(Mac)

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -25,6 +25,40 @@ void operator delete(void* obj) noexcept {
 }
 // #@@range_end(placement_new)
 
+const PixelColor kDesktopBGColor{45, 118, 237};
+const PixelColor kDesktopFGColor{255, 255, 255};
+
+// #@@range_begin(mouse_cursor_shape)
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
+  "@              ",
+  "@@             ",
+  "@.@            ",
+  "@..@           ",
+  "@...@          ",
+  "@....@         ",
+  "@.....@        ",
+  "@......@       ",
+  "@.......@      ",
+  "@........@     ",
+  "@.........@    ",
+  "@..........@   ",
+  "@...........@  ",
+  "@............@ ",
+  "@......@@@@@@@@",
+  "@......@       ",
+  "@....@@.@      ",
+  "@...@ @.@      ",
+  "@..@   @.@     ",
+  "@.@    @.@     ",
+  "@@      @.@    ",
+  "@       @.@    ",
+  "         @.@   ",
+  "         @@@   ",
+};
+// #@@range_end(mouse_cursor_shape)
+
 // #@@range_begin(buf)
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
@@ -61,20 +95,40 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       break;
   }
 
-  for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
-    for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-      pixel_writer->Write(x, y, {255, 255, 255});
+  const int kFrameWidth = frame_buffer_config.horizontal_resolution;
+  const int kFrameHeight = frame_buffer_config.vertical_resolution;
+
+  // #@@range_begin(draw_desktop)
+  FillRectangle(*pixel_writer,
+                {0, 0},
+                {kFrameWidth, kFrameHeight},
+                kDesktopBGColor);
+  FillRectangle(*pixel_writer,
+                {0 + 20, kFrameHeight - 60},
+                {kFrameWidth - 20 * 2, 60 - 5},
+                {61, 68, 77});
+  // DrawRectangle(*pixel_writer,
+  //               {10, kFrameHeight - 40},
+  //               {30, 30},
+  //               {160, 160, 160});
+  
+  console = new(console_buf) Console{
+    *pixel_writer, kDesktopFGColor, kDesktopBGColor
+  };
+  printk("Welcome to MikanOS! by yuu\n");
+  // #@@range_end(draw_desktop)
+
+  // #@@range_begin(draw_mouse_cursor)
+  for (int dy = 0; dy < kMouseCursorHeight; ++dy) {
+    for (int dx = 0; dx < kMouseCursorWidth; ++dx) {
+      if (mouse_cursor_shape[dy][dx] == '@') {
+        pixel_writer->Write(200 + dx, 100 + dy, {0, 0, 0});
+      } else if (mouse_cursor_shape[dy][dx] == '.') {
+        pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
+      }
     }
   }
-  
-  // #@@range_begin(new_console)
-  console = new(console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
-  // #@@range_end(new_console)
+  // #@@range_end(draw_mouse_cursor)
 
-  // #@@range_begin(use_printk)
-  for (int i = 0; i < 27; ++i) {
-    printk("printk: %d\n", i);
-  }
-  // #@@range_end(use_printk)
-  while (1) __asm__("hlt");
+  while (true) __asm__("hlt");
 }
