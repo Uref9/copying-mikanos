4e6bf0b85c5a7797e73d0619af07792001d0b0de
YusukeSK <62171006+YusukeSK@users.noreply.github.com>
2021-09-22 18:02:01 +0900

    15d faster drawing

--- kernel/layer.cpp
+++ kernel/layer.cpp
@@ -70,12 +70,20 @@ void LayerManager::Draw(const Rectangle<int>& area) const {
 }
 
 void LayerManager::Draw(unsigned int id) const {
+  Draw(id, {{0, 0}, {-1, -1}});
+}
+
+void LayerManager::Draw(unsigned int id, Rectangle<int> area) const {
   bool draw = false;
   Rectangle<int> window_area;
   for (auto layer : layer_stack_) {
     if (layer->ID() == id) {
       window_area.size = layer->GetWindow()->Size();
       window_area.pos = layer->GetPosition();
+      if (area.size.x >= 0 || area.size.y >= 0) {
+        area.pos = area.pos + window_area.pos;
+        window_area = window_area & area;
+      }
       draw = true;
     }
     if (draw) {
@@ -84,6 +92,7 @@ void LayerManager::Draw(unsigned int id) const {
   }
   screen_->Copy(window_area.pos, back_buffer_, window_area);
 }
+
 
 void LayerManager::Move(unsigned int id, Vector2D<int> new_pos) {
   auto layer = FindLayer(id);
@@ -258,6 +267,9 @@ void ProcessLayerMessage(const Message& msg) {
     break;
   case LayerOperation::Draw:
     layer_manager->Draw(arg.layer_id);
+    break;
+  case LayerOperation::DrawArea:
+    layer_manager->Draw(arg.layer_id, {{arg.x, arg.y}, {arg.w, arg.h}});
     break;
   }
 }
