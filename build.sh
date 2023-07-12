3697206be737a1cce951257a8d9039e8af54a07a
Uref <youbowtou6@gmail.com>
2023-07-12 08:40:22 +0000

    Restore before day20d but dosent work apps

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+RUN=$1
+
+source ~/osbook/devenv/buildenv.sh
+/workspaces/mikanos-devcontainer/mikanos/build.sh $RUN
--- diff.sh
+++ diff.sh
@@ -4,15 +4,15 @@ DIR=$1
 
 if [ "$DIR" = "kernel" ]; then
   cd /workspaces/mikanos-devcontainer/copying-mikanos/kernel
-  make
+  make clean
   make dclean
   cd -
-elif [ "$DIR" = "MikanLoaderPkg/Main.c" ]; then
-  cd ~/edk2
-  source edksetup.sh
-  build
-  cd -
 fi
+# elif [ "$DIR" = "MikanLoaderPkg/Main.c" ]; then
+#   cd ~/edk2
+#   source edksetup.sh
+#   build
+#   cd -
 
 
 # apps make?
